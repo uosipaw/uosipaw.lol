f4c4a46a500328e795d21d8fdb754016ee6a73c3
uosipaw <uosipaw@outlook.com>
2025-03-05 17:46:17 -0500

    uosipaw

---
+++ script.js
@@ -0,0 +1,7 @@
+function startAnimation() {
+  // Hide only the button, NOT the whole div
+  document.querySelector(".playbutton").style.display = "none";
+
+  // Show the video instantly
+  document.getElementById("animationVideo").style.display = "block";
+}
